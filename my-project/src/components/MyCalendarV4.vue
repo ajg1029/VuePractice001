a162696a91819a5a172a37ed4ec6c389724e5eb8
Deon <urban.seonbi.deon@gmail.com>
2023-01-26 19:36:08 +0900

    resa 이름 변경

--- my-project/src/components/MyCalendarV4.vue
+++ my-project/src/components/MyCalendarV4.vue
@@ -4,13 +4,24 @@
             <button @click="testMethod()">test</button>
             <div>currentDatetime : {{ currentDatetime }}</div>
             <div>targetDatetime : {{ targetDatetime }}</div>
-            <button v-text="'<'" @click="setPrevDate()"></button>
-            <button v-text="'>'" @click="setNextDate()"></button>
+            <div>
+                <button v-text="'<'" @click="setPrevDate()"></button>
+                {{ resaDate }}
+                <button v-text="'>'" @click="setNextDate()"></button>
+            </div>
         </header>
         <section>
 
         </section>
         <section>
+            <table>
+                <thead>
+                    <tr>
+                        <th>{{ resaMonth }}월</th>
+                        <th v-for="(day, idx) in resaDays" :key="idx">{{ day }}요일</th>
+                    </tr>
+                </thead>
+            </table>
 
         </section>
     </div>
@@ -23,37 +34,35 @@ export default {
     data() {
         return{
 
-            currentDatetime: new Date(),
+            currentDatetime: new Date(), // 실시간 현재 시간
 
-            firstDay: null,
-            weekArrangeType: null,
-            weekNumberingType: null,
+            firstDay: null, // 한 주의 시작 요일(1, 2, 3, 4, 5, 6, 0)
+            weekArrangeType: null, // 전체(+0) 또는 과반수(+3)
+            weekNumberingType: null, // mw 또는 yw
 
-            targetDatetime: null,
+            targetDatetime: null, // 선택 날짜, Date() 객체, 00시00분
 
-            iresaiDays: null,
-            iresaiDate: null,
-            iresaiMWeek: null,
-            iresaiYWeek: null,
-            iresaiMonth: null,
-            iresaiYear: null,
-            iresaiCalendar: null,
-            isDateIncluded: true,
-            
-            
-            
+            resaDays: null, // 달력 상단의 요일 순서, firstDay 에 따라 결정
+            resaDate: null, // targetDatetime의 YYYY-MM-DD 포맷
+            resaMWeek: null, // 월 기준의 week numbering
+            resaYWeek: null, // 연 기준의 week numbering
+            resaMonth: null, // targetDatetime 기준의 월이 아닌, resa 기준의 월
+            resaYear: null, // 마찬가지로 resa 기준 연도
+            resaCalendar: null, // resa 달력의 각 날짜가 들어간 배열
+            isDateIncluded: true, // targetDatetime 이 바뀌었을 때 resaMonth 를 벗어나면 false
+
         }
     },
     created() {
         this.updateCurrentDatetime()
         this.firstDay = 1
-        this.weekArrangeType = 3
-        this.weekNumberingType = 'mw' // 'yw' 한 해의 몇 번째 주인지 / 'mw' 한 달의 몇 번째 주인지
+        this.weekArrangeType = +3
+        this.weekNumberingType = 'mw'
         this.targetDatetime = new Date(this.currentDatetime.getFullYear(), this.currentDatetime.getMonth(), this.currentDatetime.getDate())
     },
     watch: {
         targetDatetime(val) {
-            this.iresaiDate = dayjs(val).format('YYYY-MM-DD')
+            this.resaDate = dayjs(val).format('YYYY-MM-DD')
 
             // iresaiDate: null, // YYYY-MM-DD 포맷 문자열 업데이트
             // iresaiMWeek: null, // 주-월넘버링
@@ -61,6 +70,7 @@ export default {
             // iresaiMonth: null, // 달력 월 - 기존과 다르면 isDateIncluded false
             // iresaiYear: null, // 달력 해
             // iresaiCalendar: [], // 달력
+            this.setResaCalendar(val)
         },
         isDateIncluded(val) {
             if (val === false) {
@@ -94,15 +104,17 @@ export default {
         setNextDate() {
             this.targetDatetime = new Date(this.targetDatetime.getFullYear(), this.targetDatetime.getMonth(), this.targetDatetime.getDate() + 1)
         },
-        setIresaiCalendar(datetime) {
-            this.iresaiDays = []
-            this.iresaiCalendar = []
+        setResaCalendar(datetime) {
+            this.resaDays = []
+            this.resaCalendar = []
 
             let benchmarkDay = this.firstDay + this.weekArrangeType
 
+            this.resaCalendar = 
+
             let tempDay = this.firstDay
             for (let i = 0; i < 7; i++) {
-                this.iresaiDays.push(this.convertDayToKor(tempDay%7))
+                this.resaDays.push(this.convertDayToKor(tempDay%7))
                 tempDay++
             }
 
@@ -111,18 +123,18 @@ export default {
             // year week numbering 과 month week numbering 부터 세어야 한다...
             let yearLength = 0
             // target 의 month 에 도달했을 때는 month week numbering을 센다...
-            for (let i = 0; i < 12; i++) {
-                if (i === datetime.getMonth()) {
-                    for (let j = 0; j < new Date(datetime.getFullYear(), i + 1, 0).getDate(); j++) {
+            // for (let i = 0; i < 12; i++) {
+            //     if (i === datetime.getMonth()) {
+            //         for (let j = 0; j < new Date(datetime.getFullYear(), i + 1, 0).getDate(); j++) {
                         
-                    }
-                } else {
-                    for (let j = 0; j < new Date(datetime.getFullYear(), i + 1, 0).getDate(); j++) {
+            //         }
+            //     } else {
+            //         for (let j = 0; j < new Date(datetime.getFullYear(), i + 1, 0).getDate(); j++) {
                         
-                    }
-                }
-            }
-
+            //         }
+            //     }
+            // }
+            console.log(datetime)
             
             
             
